64b556c9ed19ca1882953d503b90f9ff7d5b7c62
Robert deLaguna <rjdelaguna@yahoo.com>
2023-05-31 13:40:12 -0700

    Test 4 methods with notes

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,17 @@ strings = ["robert", "felicia", "brad", "charles", "megan"]
 integers = [0, 1, 2, 3, 4]
 floats = [0.0, 0.1, 0.2, 0.3, 0.4]
 booleans = [true, false, true, false, true]
+# .pop(2) will remove the last 2 elements in the strings array
+# charles and megan will be gone
+strings.pop(2)
+p strings
+# .push(5, 6) will add elements [5, 6] to the end of the integers array
+integers.push(5, 6)
+p integers
+# .shift(3) will remove the first 3 elements in the floats array
+# only [0.3, 0.4] will remain
+floats.shift(3)
+p floats
+# .unshift(false) will add the false element to index position 0
+booleans.unshift(false)
+p booleans
